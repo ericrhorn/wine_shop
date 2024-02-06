4917c9bb64ae82405786fc71b7df8bc7ac3a3b76
Eric Horn <hornericr@me.com>
2024-02-05 17:53:11 -0800

    home page and store

--- wine_shop/src/components/Home.js
+++ wine_shop/src/components/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div style={{ width: '90%', margin: '0 auto' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', border: 'solid black 1px' }}>
+        <Grid container justifyContent="center" alignItems="center" spacing={2}>
+          <Grid item xs={8}>
+            <h1 style={{ textAlign: 'center', fontSize: '50px' }}>The Wine Club</h1>
+            <p style={{lineHeight: '2', fontSize: '20px'}}>Sed quid ages tandem, si utilitas ab amicitia, ut fit saepe, defecerit? Hoc enim constituto in philosophia constituta sunt omnia. Quamquam te quidem video minime esse deterritum. Sic enim censent, oportunitatis esse beate vivere. Quid enim tanto opus est instrumento in optimis artibus comparandis? Omnium enim rerum principia parva sunt, sed suis progressionibus usa augentur nec sine causa; Hoc Hieronymus summum bonum esse dixit.</p>
+          </Grid>
+        </Grid>
+      </Box>
+      
+    </div>
   )
 }
 
--- wine_shop/src/components/Store.js
+++ wine_shop/src/components/Store.js
@@ -1,23 +1,178 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
-  color: theme.palette.text.secondary,
-}));
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+
+import wineBottle from '../assets/wine_bottle.jpeg'
+
+// import { styled } from '@mui/material/styles';
+// import Box from '@mui/material/Box';
+// import Paper from '@mui/material/Paper';
+// import Grid from '@mui/material/Grid';
+
+// const Item = styled(Paper)(({ theme }) => ({
+//   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+//   ...theme.typography.body2,
+//   padding: theme.spacing(1),
+//   textAlign: 'center',
+//   color: theme.palette.text.secondary,
+// }));
 
 
 
 function Store() {
   return (
-    <div style={{margin: '100px 100px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <Box sx={{ flexGrow: 1}}>
+    <div style={{ margin: '100px auto', maxWidth: '1200px', display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(250px, 1fr))', gap: '20px', padding: '0px 50px 50px' }}>
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={wineBottle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Wine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Delicious red wine from napa valley
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={wineBottle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Wine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Delicious red wine from napa valley
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={wineBottle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Wine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Delicious red wine from napa valley
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={wineBottle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Wine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Delicious red wine from napa valley
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={wineBottle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Wine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Delicious red wine from napa valley
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={wineBottle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Wine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Delicious red wine from napa valley
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={wineBottle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Wine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Delicious red wine from napa valley
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={wineBottle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Wine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Delicious red wine from napa valley
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={wineBottle}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Red Wine
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Delicious red wine from napa valley
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    
+      {/* <Box sx={{ flexGrow: 1}}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <Item>xs=8</Item>
@@ -35,7 +190,7 @@ function Store() {
           <Item>xs=6 md=8</Item>
         </Grid>
         </Grid>
-      </Box>
+      </Box> */}
     </div>
       )
 }
