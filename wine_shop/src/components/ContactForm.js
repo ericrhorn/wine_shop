58f0a1bb1bc37f39e679758a199249e0b6c7642c
Eric Horn <hornericr@me.com>
2024-02-12 16:21:40 -0800

    contact form styling and background

--- wine_shop/src/components/ContactForm.js
+++ wine_shop/src/components/ContactForm.js
@@ -1,73 +1,110 @@
 import React from 'react';
-import { Box, TextField, Button } from '@mui/material';
+import { Box, TextField, Button, FormControl, InputLabel, Select, MenuItem, Grid, CardHeader, CardContent, Card} from '@mui/material';
+import {Form, Field } from "formik"
+import Img from "../assets/vinyard.jpeg"
+// import '../style/contact.css'
+
 
 export const ContactForm = () => {
+
+  
+
+  const inquiries = [
+    {value: 'Wine Club', label: 'Wine Club'},
+    {value: 'Wine Club', label: 'Wine Club'},
+    {value: 'Wine Club', label: 'Wine Club'},
+    {value: 'Wine Club', label: 'Wine Club'},
+    {value: 'Wine Club', label: 'Wine Club'}
+  ]
+  
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
+    <>
+   <Box
+   display="flex"
+      flexDirection="column"
       alignItems="center"
+      justifyContent="center"
+      style={{
+        backgroundImage: `url(${Img})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        height: '800px', // Adjust the height as needed
+        // padding: '50px 0', // Add padding to ensure content is not covered by the image
+      }}
     >
-      <Box
+      {/* <Box
         component="form"
         sx={{
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
+          '& .MuiTextField-root': { m: 1, width: '700px', maxWidth: '100%' },
         }}
         noValidate
         autoComplete="off"
-        style={{ border: 'solid black 1px', maxWidth: '800px', width: '100%', textAlign: 'center', padding: '20px' }}
-      >
-        <Box display="flex" flexDirection="row" alignItems="center">
+        style={{ border: 'solid black 1px', maxWidth: '1000px', width: '100%', textAlign: 'center', padding: '20px' }}
+      > */}
+        <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        maxWidth="800px"
+        width="100%"
+        p="20px"
+        bgcolor="rgba(255, 255, 255, .7)"
+        >
           <div>
+            <div>
+              <h1>Questions?</h1>
+            </div>
             <TextField
-              required
-              id="outlined-required"
-              label="Required"
-              defaultValue="Hello World"
+              sx={{ m: 1, width: '35ch' }}
+              // required
+              id="name"
+              label="Name"
+              // defaultValue="Hello World"
             />
             <TextField
-              disabled
-              id="outlined-disabled"
-              label="Disabled"
-              defaultValue="Hello World"
+             sx={{ m: 1, width: '35ch' }}
+              // disabled
+              id="email"
+              label="Email"
+              // defaultValue="Hello World"
             />
             <TextField
+             sx={{ m: 1, width: '35ch' }}
               id="outlined-password-input"
-              label="Password"
+              label="Phone Number"
               type="password"
               autoComplete="current-password"
             />
-            <TextField
-              id="outlined-read-only-input"
-              label="Read Only"
-              defaultValue="Hello World"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <TextField
-              id="outlined-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-            <TextField id="outlined-search" label="Search field" type="search" />
-            <TextField
-              id="outlined-helperText"
-              label="Helper text"
-              defaultValue="Default Value"
-              helperText="Some important text"
-            />
-                      <div>
-                        <Button variant="outlined" >Outlined</Button>
+              <TextField
+               sx={{ m: 1, width: '35ch' }}
+                id="inquiry"
+                select
+                label="Inquiry"
+              >
+                {inquiries.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+              <TextField
+                sx={{m: 1}}
+                fullWidth
+                id="outlined-multiline-static"
+                label="Multiline"
+                multiline
+                rows={6}
+                defaultValue="Default Value"
+              />
 
           </div>
+          <div>
+            <Button variant="outlined" >Outlined</Button>
           </div>
 
         </Box>
-      </Box>
+      {/* </Box> */}
     </Box>
+    </>
   );
 };
--- wine_shop/src/components/Home.js
+++ wine_shop/src/components/Home.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
 
 import { ContactForm } from './ContactForm';
 import HomeImages from './HomeImages';
@@ -11,6 +13,16 @@ import food from '../assets/food.jpeg'
 import calendar from '../assets/calendar.jpeg'
 
 function Home() {
+
+  const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+
   return (
     <div style={{padding: '10px'}}>
       <div style={{ width: '90%', margin: '0 auto' }}>
@@ -27,56 +39,64 @@ function Home() {
         <Box sx={{ flexGrow: 1 }}>
           <Grid justifyContent="center" alignItems="center" container spacing={0} columns={{ xs: 2, sm: 4, md: 8, lg: 12 }}>
               <Grid style={{padding: '10px', textAlign: 'center'}} item xs={12} sm={6} md={3} lg={3}>
-                <div>
-                  <img src={wineClub} alt="" style={{maxWidth: '150px',}} />
-                </div>
-                <div>
-                  <h1 style={{fontFamily: 'Open Sans', textTransform: 'uppercase'}}>
-                    Wine Club
-                  </h1>
-                  <div style={{padding: '0px 25px'}}>
-                    <p>Duarum enim vitarum nobis erunt instituta capienda. Hunc vos beatum; Igitur ne dolorem quidem. Est enim effectrix multarum et magnarum voluptatum. Nec enim, dum metuit, iustus est, et certe, si metuere destiterit, non erit;</p>
+                <Item>
+                  <div>
+                    <img src={wineClub} alt="" style={{maxWidth: '150px',}} />
                   </div>
-                </div>
+                  <div>
+                    <h1 style={{fontFamily: 'Open Sans', textTransform: 'uppercase'}}>
+                      Wine Club
+                    </h1>
+                    <div style={{padding: '0px 25px'}}>
+                      <p>Duarum enim vitarum nobis erunt instituta capienda. Hunc vos beatum; Igitur ne dolorem quidem. Est enim effectrix multarum et magnarum voluptatum. Nec enim, dum metuit, iustus est, et certe, si metuere destiterit, non erit;</p>
+                    </div>
+                  </div>
+                </Item>
               </Grid>
               <Grid style={{padding: '10px',textAlign: 'center'}} item xs={12} sm={6} md={3} lg={3}>
-                <div>
-                  <img src={tasting} alt="" style={{maxWidth: '150px',}} />
-                </div>
-                <div>
-                  <h1 style={{fontFamily: 'Open Sans', textTransform: 'uppercase'}}>
-                    Tasting Room
-                  </h1>
-                  <div style={{padding: '0px 25px'}}>
-                    <p>Duarum enim vitarum nobis erunt instituta capienda. Hunc vos beatum; Igitur ne dolorem quidem. Est enim effectrix multarum et magnarum voluptatum. Nec enim, dum metuit, iustus est, et certe, si metuere destiterit, non erit;</p>
+                <Item>
+                  <div>
+                    <img src={tasting} alt="" style={{maxWidth: '150px',}} />
                   </div>
-                </div>
+                  <div>
+                    <h1 style={{fontFamily: 'Open Sans', textTransform: 'uppercase'}}>
+                      Tasting Room
+                    </h1>
+                    <div style={{padding: '0px 25px'}}>
+                      <p>Duarum enim vitarum nobis erunt instituta capienda. Hunc vos beatum; Igitur ne dolorem quidem. Est enim effectrix multarum et magnarum voluptatum. Nec enim, dum metuit, iustus est, et certe, si metuere destiterit, non erit;</p>
+                    </div>
+                  </div>
+                </Item>
               </Grid>
               <Grid style={{padding: '10px',textAlign: 'center'}} item xs={12} sm={6} md={3} lg={3}>
-                <div>
-                  <img src={food} alt="" style={{maxWidth: '150px',}} />
-                </div>
-                <div>
-                  <h1 style={{fontFamily: 'Open Sans', textTransform: 'uppercase'}}>
-                    Cuisine
-                  </h1>
-                  <div style={{padding: '0px 25px'}}>
-                    <p>Duarum enim vitarum nobis erunt instituta capienda. Hunc vos beatum; Igitur ne dolorem quidem. Est enim effectrix multarum et magnarum voluptatum. Nec enim, dum metuit, iustus est, et certe, si metuere destiterit, non erit;</p>
+                <Item>
+                  <div>
+                    <img src={food} alt="" style={{maxWidth: '150px',}} />
                   </div>
-                </div>
+                  <div>
+                    <h1 style={{fontFamily: 'Open Sans', textTransform: 'uppercase'}}>
+                      Cuisine
+                    </h1>
+                    <div style={{padding: '0px 25px'}}>
+                      <p>Duarum enim vitarum nobis erunt instituta capienda. Hunc vos beatum; Igitur ne dolorem quidem. Est enim effectrix multarum et magnarum voluptatum. Nec enim, dum metuit, iustus est, et certe, si metuere destiterit, non erit;</p>
+                    </div>
+                  </div>
+                </Item>
               </Grid>
               <Grid style={{padding: '10px',textAlign: 'center'}} item xs={12} sm={6} md={3} lg={3}>
-                <div>
-                  <img src={calendar} alt="" style={{maxWidth: '150px',}} />
-                </div>
-                <div>
-                  <h1 style={{fontFamily: 'Open Sans', textTransform: 'uppercase'}}>
-                    Book Events
-                  </h1>
-                  <div style={{padding: '0px 25px'}}>
-                    <p>Duarum enim vitarum nobis erunt instituta capienda. Hunc vos beatum; Igitur ne dolorem quidem. Est enim effectrix multarum et magnarum voluptatum. Nec enim, dum metuit, iustus est, et certe, si metuere destiterit, non erit;</p>
+                <Item>
+                  <div>
+                    <img src={calendar} alt="" style={{maxWidth: '150px',}} />
                   </div>
-                </div>
+                  <div>
+                    <h1 style={{fontFamily: 'Open Sans', textTransform: 'uppercase'}}>
+                      Book Events
+                    </h1>
+                    <div style={{padding: '0px 25px'}}>
+                      <p>Duarum enim vitarum nobis erunt instituta capienda. Hunc vos beatum; Igitur ne dolorem quidem. Est enim effectrix multarum et magnarum voluptatum. Nec enim, dum metuit, iustus est, et certe, si metuere destiterit, non erit;</p>
+                    </div>
+                  </div>
+                </Item>
               </Grid>
           </Grid>
         </Box>
@@ -84,7 +104,7 @@ function Home() {
       <div>
         <HomeImages/>
       </div>
-      <div>
+      <div >
         <ContactForm/>
       </div>
 
